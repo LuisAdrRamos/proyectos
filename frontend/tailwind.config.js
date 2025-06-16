d318ffb01edda873002174bf1603613dd54406c2
Luis Adrian Ramos <luchoramos042004@gmail.com>
2025-06-16 17:00:02 -0500

    estilos

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},                    // Aquí puedes extender colores, fuentes, etc.
+    },
+    plugins: [],                     // Plugins opcionales (como forms, typography, etc.)
+}
